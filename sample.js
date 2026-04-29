6a150f81edc330b560600b8ea9e63a032718025c
ShawnMat <shawn.daniel1919@gmail.com>
2026-04-29 07:59:25 +0530

    carousel changes - not working

---
+++ sample.js
@@ -0,0 +1,56 @@
+const images = document.querySelectorAll(".images img");
+const prevBtn = document.querySelector(".prev");
+const nextBtn = document.querySelector(".next");
+const dotsContainer = document.querySelector(".dots");
+
+let index = 0;
+let interval;
+
+
+images.forEach((_, i) => {
+  const dot = document.createElement("span");
+  dot.addEventListener("click", () => showSlide(i));
+  dotsContainer.appendChild(dot);
+});
+
+const dots = document.querySelectorAll(".dots span");
+
+function showSlide(i) {
+  images[index].classList.remove("active");
+  dots[index].classList.remove("active");
+
+  index = i;
+
+  images[index].classList.add("active");
+  dots[index].classList.add("active");
+}
+
+
+function nextSlide() {
+  let i = (index + 1) % images.length;
+  showSlide(i);
+}
+
+function prevSlide() {
+  let i = (index - 1 + images.length) % images.length;
+  showSlide(i);
+}
+
+nextBtn.addEventListener("click", nextSlide);
+prevBtn.addEventListener("click", prevSlide);
+
+
+function startAutoSlide() {
+  interval = setInterval(nextSlide, 3000);
+}
+
+function stopAutoSlide() {
+  clearInterval(interval);
+}
+
+
+document.querySelector(".slider").addEventListener("mouseenter", stopAutoSlide);
+document.querySelector(".slider").addEventListener("mouseleave", startAutoSlide);
+
+showSlide(index);
+startAutoSlide();
